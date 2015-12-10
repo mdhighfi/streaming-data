5f19ba36a186ccf83a025da3968467af27f9595f
mdhighfi <matthew.highfield@gmail.com>
2015-12-10 10:55:46 -0800

    extract data from stream

--- compute_stats.rb
+++ compute_stats.rb
@@ -1,12 +1,23 @@
 #!/usr/bin/ruby
 
-require 'csv'
 require 'open3'
 
-stdin, stdout, stderr = Open3.popen3("#{Dir.getwd}/../generator", "4")
+types = []
+names = []
+header = true
 
-p "standard output: #{stdout.gets}"
-p "standard output: #{stdout.gets}"
-p "standard output: #{stdout.gets}"
-p "standard output: #{stdout.gets}"
-p "standard output: #{stdout.gets}"
+IO.popen("./generator 10") do |stream|
+  while line = stream.gets
+    values = line.split(',').map(&:strip)
+    if header
+      values.each_with_index do |val, col|
+        _, names[col], types[col] = val.match(/(\w+) \((\w+)/).to_a
+      end
+      header = false
+    else
+      values.each_with_index do |val, col|
+        p "#{names[col]}: #{val}, (#{types[col]})"
+      end
+    end
+  end
+end
