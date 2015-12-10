1e24f500344ab23ce1a4d41b6d4eef98ea8ed2bc
mdhighfi <matthew.highfield@gmail.com>
2015-12-10 14:15:55 -0800

    Add some stats

--- compute_stats.rb
+++ compute_stats.rb
@@ -16,20 +16,25 @@ class Table
     @columns[i][j] = val
   end
 
-  def define_columns(values)
-    values.each_with_index do |val, col|
-      _, names[col], types[col] = val.match(/(\w+) \((\w+)/).to_a
-      case types[col]
+  def define_columns(header)
+    header.each_with_index do |val, col|
+      _, name, type = val.match(/(\w+) \((\w+)/).to_a
+      case type
       when 'text'
-        table << TextColumn.new(names[col], col)
+        @columns << TextColumn.new(name, col)
       when 'number'
-        table << NumberColumn.new(names[col], col)
+        @columns << NumberColumn.new(name, col)
       else
-        table << Column.new(names[col], col)
+        @columns << Column.new(name, col)
       end
     end
   end
 
+  def stats
+    @columns.map do |col|
+      col.stats
+    end
+  end
 end
 
 class Column
@@ -40,56 +45,61 @@ class Column
     @null_count = 0
   end
 
-  def update_stats(row)
-    # implemented in child classes
+  def update_stats(val)
+    @null_count += 1 if val == ''
   end
 
+  def
 end
 
 class NumberColumn < Column
   def initialize(name, num)
-    super(name, num)
+    super
     @min = 0
     @max = 0
     @mean = 0
   end
 
-  def update_stats(row)
+  def update_stats(val)
+    super
+    @min = val if val < @min
+    @max = val if val > @max
   end
 end
 
 class TextColumn < Column
   def initialize(name, num)
-    super(name, num)
-    @min = ''
-    @max = ''
+    super
+    @min_length = ''
+    @max_length = ''
     @mean_length = 0
   end
 
-  def update_stats(row)
+  def update_stats(val)
+    super
+    length = val.length
     @count += 1
-    @null_count += 1 if
+    @mean_length = ((@count -1) * @mean_length + length) / @count
+    @min_length = length if length < @min_length
+    @max_length = length if length > @max_length
   end
-
 end
-
-
-types = []
-names = []
-header = true
 
 
 
 IO.popen("./generator 10") do |stream|
-  table = []
+  table = Table.new
+  is_header? = true
   while line = stream.gets
-    values = line.split(',').map(&:strip)
-    if header
-      define_columns
+    line.split!(',').map!(&:strip)
+    if is_header?
+      table.define_columns(line)
+      is_header? = false
     else
-      values.each_with_index do |val, col|
-        table[col].update_stats()
+      line.each_with_index do |val, col|
+        table[col].update_stats(val)
       end
     end
   end
+  p table.stats
 end
